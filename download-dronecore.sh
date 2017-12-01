faf3fecadc5617cf357f26b28a74fddba6a99b8d
Julian Oes <julian@oes.ch>
2017-12-01 16:12:21 -0500

    Added script to download DroneCore zip from s3

---
+++ download-dronecore.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e # fail on errors
+
+dronecore_filename=DroneCore-iOS-v0.9.0-Release.zip
+unzip_dir=Yuneec_SDK_iOS/libs/Yuneec-SDK
+
+downloaded=false
+if [ ! -f $dronecore_filename ]; then
+    echo "Downloading DroneCore..."
+    curl --progress-bar -O https://s3.eu-central-1.amazonaws.com/7a385c3b-88a9-2913-f590-6eecf3be1731/yuneec-sdk/ios/$dronecore_filename;
+    downloaded=true
+fi
+
+if [ ! -d Yuneec_SDK_iOS/libs/ -o downloaded=true ]; then
+    echo "Unzipping..."
+    mkdir -p $unzip_dir
+    unzip -o $dronecore_filename -d $unzip_dir
+fi
